c1a1e2fc92019320bd5d6489f12d66312e52e1d9
Lenard Roy Arellano <LArellano@msi-ecs.com.ph>
2026-03-14 15:07:18 +0800

    first commit, install dependencies and electron

---
+++ electron/main.ts
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../.next/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
